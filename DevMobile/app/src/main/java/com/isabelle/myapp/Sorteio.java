92124acdb99242695b3843964dc7143a2af9bce7
isabellemorales <isabellemoralesp@gmail.com>
2026-08-07 22:22:02 -0300

    feat: add android intro labs (hello world, app icon, sorteio)

---
+++ DevMobile/app/src/main/java/com/isabelle/myapp/Sorteio.java
@@ -0,0 +1,38 @@
+package com.isabelle.myapp;
+
+import static com.isabelle.myapp.R.id.resultado;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class Sorteio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sorteio);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button bt = findViewById(R.id.btSorteio);
+        TextView resultado = findViewById(R.id.resultado);
+        bt.setOnClickListener(v -> {
+            int i = new Random().nextInt(60) + 1;
+            resultado.setText("Resultado: " + i);
+        });
+    }
+}
